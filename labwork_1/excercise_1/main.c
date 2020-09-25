63ad7d6ec9f36223689427a6e49101c1d2e594db
nmikhalchenko <Halfer2000@gmail.com>
2020-09-26 00:00:53 +0300

    Complete excercises 1 and 2 of the 1st labwork

---
+++ labwork_1/excercise_1/main.c
@@ -0,0 +1,28 @@
+// Вариант 5
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+#define MULTIPLIER 2
+#define EXPONENT 8
+
+int main(void)
+{
+    // Вызов этой функции не изменяет
+    // ход выполнения программы.
+    //
+    //setlocale(LC_ALL, "Russian");
+
+    float x;
+    puts("Введите число:");
+    scanf("%f", &x);
+
+    printf(
+        "Число %f в степени %i, умноженное на %i: %f\n", 
+        x, 
+        EXPONENT, 
+        MULTIPLIER, 
+        powf(x, EXPONENT) * MULTIPLIER);
+}
---
+++ labwork_1/excercise_2/main.c
@@ -0,0 +1,38 @@
+// Вариант 5
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define VALUE_TO_COMPARE_TO -182.038
+#define X -15.246
+#define Y 0.04642
+#define Z 21.0
+
+double deg2rad(double deg)
+{
+    return deg * M_PI / 180.0;
+}
+
+double expression(double x, double y, double z)
+{
+    double result = 
+        log(pow(y, -sqrt(abs(x)))) *
+        (x - y / 2.0) + 
+        pow(deg2rad(sin(atan(deg2rad(z)))), 2);
+
+    
+
+    return result;
+}
+
+int main(void)
+{
+    // При запуске было равно -182.038 .
+    printf("Значение для сравнения: %g\n", 
+        VALUE_TO_COMPARE_TO);
+
+    // При запуске было равно -181.553 .
+    printf("Значение выражения: %g\n",
+        expression(X, Y, Z));
+}
