2a3526a019b179a1f85e438c0bf16a64b3277e13
nmikhalchenko <Halfer2000@gmail.com>
2020-10-04 12:09:57 +0300

    Complete labwork 3

---
+++ labwork_3/excercise_1/main.c
@@ -0,0 +1,54 @@
+// Вариант 5
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int isNaturalNumber(float n)
+{
+    if (n <= 0.0f)
+        return 0;
+
+    float preDot = truncf(n);
+    if ((n - preDot) > 0.0f)
+        return 0;
+
+    return 1;
+}
+
+int getPopulationTripleYear(int n)
+{
+    int counter = 0;
+    for (int i = 0; i < n * 2; i++)
+    {
+        ++counter;
+    }
+
+    return counter;
+}
+
+int main(void)
+{
+    puts("Население города ежегодно увеличивается на 1 / n.");
+    puts("Введите значение натурального числа n:");
+
+    float input = 0.0f;
+    scanf("%f", &input);
+
+    printf("Вы ввели значение: %f\n", input);
+
+    int checkedInput = 0;
+
+    if (!isNaturalNumber(input))
+    {
+        puts("Ошибка: введено ненатуральное число!");
+        return -1;
+    }
+
+    checkedInput = (int)input;
+
+    printf("Население города утроится через %i лет.\n",
+        getPopulationTripleYear(checkedInput));
+
+    return 0;
+}
