4706177bddb3d393632cab13c83cd00ae612228c
nmikhalchenko <Halfer2000@gmail.com>
2020-10-08 23:44:37 +0300

    Solve excercise 2 of labwork 4, reuse the code from excercise 2 in 1

---
+++ labwork_4/excercise_1/unique.h
@@ -0,0 +1,10 @@
+#ifndef UNIQUE_H
+#define UNIQUE_H
+
+#include <stdlib.h>
+
+void extractUnique(int* inputArr, size_t inputArrSize, 
+                   int** outputArr, size_t* outputArrSize);
+void printUnique(int* arr, size_t size);
+
+#endif
--- labwork_4/excercise_2/main.c
+++ labwork_4/excercise_2/main.c
@@ -3,7 +3,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "unique.h"
+
+#define ARRSIZE 5
+
 int main(void)
 {
+    // Наш "какой-то" массив.
+    int in[ARRSIZE] = {1, 2, 2, 3, 4};
+
+    int* out = NULL;
+    size_t outSize = 0;
+
+    extractUnique(in, ARRSIZE, &out, &outSize);
+
+    for (size_t i = 0; i < outSize; i++)
+        printf("%i\n", out[i]);
+
+    free(out);
+    out = NULL;
+
     return 0;
 }
