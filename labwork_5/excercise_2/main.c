5a5cd4f9dd87bb04dae05c21b03c09f556f2796d
nmikhalchenko <Halfer2000@gmail.com>
2020-11-18 17:55:46 +0300

    Complete labwork 5

--- labwork_5/excercise_2/main.c
+++ labwork_5/excercise_2/main.c
@@ -1,9 +1,45 @@
 //Вариант 5
 
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+int countColumnsWithoutZeroes(int rowCount, int colCount, int matrix[rowCount][colCount])
+{
+    int count = 0;
+
+    for (int j = 0; j < colCount; j++)
+    {
+        bool hasZeroes = false;
+        for (int i = 0; i < rowCount; i++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                hasZeroes = true;
+                break;
+            }
+        }
+        count += hasZeroes ? 0 : 1;
+    }
+    
+    return count;
+}
+
 int main(void)
 {
+    #define ROWS (5)
+    #define COLS (4)
+
+    int matrix[ROWS][COLS] =
+    {
+        {2, 3, 4, 1},
+        {1, 0, 3, 0},
+        {1, 0, 2, 0},
+        {2, 3, 4, 5},
+        {1, 2, 5, 1}
+    };
+
+    printf("Column without zeroes count: %i\n", countColumnsWithoutZeroes(ROWS, COLS, matrix));
+
     return 0;
 }
--- labwork_5/excercise_3/main.c
+++ labwork_5/excercise_3/main.c
@@ -1,9 +1,90 @@
 //Вариант 5
 
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+int indexOfFirstRowWithPositiveElement(int rowCount, int colCount, int matrix[rowCount][colCount])
+{
+    int positiveIndex = -1;
+
+    for (int i = 0; i < rowCount; i++)
+    {
+        bool hasPositive = false;
+        for (int j = 0; j < colCount; j++)
+        {
+            if (hasPositive = (matrix[i][j] > 0))
+            {
+                positiveIndex = i;
+                break;
+            }
+        }
+        if (hasPositive)
+            break;
+    }
+
+    return positiveIndex;
+}
+
+void flipSigns(int length, int row[length])
+{
+    for (int i = 0; i < length; i++)
+        row[i] = -row[i];
+}
+
+void printRow(int length, int row[length])
+{
+    printf("{ ");
+    for (int i = 0; i < length; i++)
+    {
+        printf("%i", row[i]);
+        if (i < length - 1)
+            printf(" ");
+    }
+    printf(" }");
+}
+
 int main(void)
 {
+    #define ROWS (5)
+    #define COLS (4)
+
+    int matrix[ROWS][COLS] =
+    {
+        { 0, 0, 0, 0},
+        {-1, 0,-1, 0},
+        {-1, 1, 0, 1},
+        { 0, 0, 0, 0},
+        {-1,-1,-1,-1}
+    };
+
+    int positiveIndex = indexOfFirstRowWithPositiveElement(ROWS, COLS, matrix);
+
+    if (positiveIndex >= 0)
+    {
+        printf("The index of the first row with a positive element: %i\n", positiveIndex + 1);
+        printf("The row itself: ");
+        printRow(COLS, matrix[positiveIndex]);
+        puts("");
+
+        int prevRowIndex = positiveIndex - 1;
+        if (prevRowIndex >= 0)
+        {
+            int* flippedRow = matrix[positiveIndex - 1];
+            printf("Previous row: ");
+            printRow(COLS, flippedRow);
+            puts("");
+
+            flipSigns(COLS, flippedRow);
+            printf("Previous row with element signs flipped: ");
+            printRow(COLS, flippedRow);
+            puts("");
+        }
+        else
+            printf("This row has no previous row.");
+    }
+    else
+        puts("This matrix has no rows with positive elements.");
+
     return 0;
 }
